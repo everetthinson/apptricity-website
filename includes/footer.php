2421afffd974a94071ab10c8c2ca68091b8ba198
Everett Hinson <everett@hinsondmc.com>
2016-01-20 16:48:56 -0600

    Commit to Remote

---
+++ includes/footer.php
@@ -0,0 +1,125 @@
+
+
+<span id="footerTOP">
+
+			<div class="footer-toplink" align="center">
+            <br><br>
+            <a href="#">Start of page</a>
+            <i style="color:#000;" class="fa fa-arrow-circle-o-up"></i>
+            </div>
+
+</span>
+
+<footer id="footer">
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 margin30">
+                        <div class="footer-col">
+                            <h3>About Apptricity</h3>
+                            <p>Apptricity is a leading global provider of innovative mobile enterprise solutions combining a state-of-the-art user experience, forward-thinking designs and innovative technologies, all expeditiously delivered across multiple platforms. We address the mission-critical supply chain management (SCM) and spend management demands of Fortune 500 companies, small and midsize enterprises and government organizations.</p>
+                            <ul class="list-inline footer-social">
+                                <li>
+                                    <a href="https://www.facebook.com/Apptricity" class="social-icon si-dark si-gray-round si-colored-facebook">
+                                        <i class="fa fa-facebook"></i>
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/apptricity" class="social-icon si-dark si-gray-round si-colored-twitter">
+                                        <i class="fa fa-twitter"></i>
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://plus.google.com/+Apptricity/posts" class="social-icon si-dark si-gray-round si-colored-google-plus">
+                                        <i class="fa fa-google-plus"></i>
+                                        <i class="fa fa-google-plus"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.youtube.com/user/Apptricity" class="social-icon si-dark si-gray-round si-colored-youtube">
+                                        <i class="fa fa-youtube"></i>
+                                        <i class="fa fa-youtube"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/company/apptricity" class="social-icon si-dark si-gray-round si-colored-linkedin">
+                                        <i class="fa fa-linkedin"></i>
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>                        
+                    </div>
+                    
+                    <!--footer col-->
+                    <div class="col-md-3 col-sm-6 margin30">
+                        <div class="footer-col">
+                            <h3>Contact</h3>
+
+                            <ul class="list-unstyled contact">
+                            	<li><p><b>Headquarters</b></p></li>
+                                <li><p><strong><i class="fa fa-map-marker"></i></strong>5605 N. MacArthur Blvd., #900
+                                <br>Irving, TX 75038 USA</p></li> 
+                                <li><p><strong><i class="fa fa-envelope"></i></strong><a href="#">info@apptricity.com</a></p></li>
+                                <li> <p><strong><i class="fa fa-phone"></i></strong>214.596.0601</p></li>
+                                <li> <p><strong><i class="fa fa-phone"></i></strong>800.693.2193</p></li>
+                            </ul>
+                        </div>                        
+                    </div>
+                    
+                    <!--footer col-->
+                    <div class="col-md-3 col-sm-6 margin30">
+                        <div class="footer-col">
+                            <h3>Our Products</h3>
+                            <ul class="list-inline f2-work">
+                                <li><a href="../apptricity-asset-management.html"><img src="../img/icons/icon-sce-asset-management-w.svg" class="img-responsive" alt="link to Apptricity's Asset Management page" height="32" width="32"></a></li>
+                                
+                                <li><a href="../apptricity-field-services.html"><img src="../img/icons/icon-sce-field-services-w.svg" class="img-responsive" alt="link to Apptricity's Field Services page"></a></li>
+                                
+                                <li><a href="../apptricity-inventory-management.html"><img src="../img/icons/icon-sce-inventory-management-w.svg" class="img-responsive" alt="link to Apptricity's Inventory Management page"></a></li>
+                                
+                                <li><a href="../apptricity-invoice-management.html"><img src="../img/icons/icon-spend-invoice-management-w.svg" class="img-responsive" alt="link to Apptricity's Invoice Management page"></a></li>
+                                
+                                <li><a href="../apptricity-procure-to-pay.html"><img src="../img/icons/icon-spend-procure-to-pay-w.svg" class="img-responsive" alt="link to Apptricity's Procure-To-Pay page"></a></li>
+                                
+                                <li><a href="../apptricity-purchasing.html"><img src="../img/icons/icon-spend-purchasing-w.svg" class="img-responsive" alt="link to Apptricity's Purchasing page"></a></li>
+                                
+                                <li><a href="../apptricity-travel-and-expense-management.html"><img src="../img/icons/icon-spend-t-and-e-w.svg" class="img-responsive" alt="link to Apptricity's Travel &amp; Expense Management page"></a></li>
+                                
+                                <li><a href="../apptricity-warehouse-management.html"><img src="../img/icons/icon-sce-warehouse-w.svg" class="img-responsive" alt="link to Apptricity's Warehouse solution page"></a></li>
+                                
+                                <li><a href="../apptricity-work-order-management.html"><img src="../img/icons/icon-sce-work-order-w.svg" class="img-responsive" alt="link to Apptricity's Work Order solution page"></a></li>
+                            </ul>
+                        </div>                        
+                    </div>
+                    
+                    
+                    <!--footer col-->
+                    <div class="col-md-3 col-sm-6 margin30">
+                        <div class="footer-col">
+                            <h3>Newsletter</h3>
+                            <p>Subscribe Here</p>
+                            <form role="form" class="subscribe-form">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Enter email to subscribe">
+                                    <span class="input-group-btn">
+                                        <button class="btn  btn-theme-dark btn-lg" type="submit">Ok</button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>                        
+                    </div>
+                    <!--footer col-->
+
+                </div>
+
+		<div class="row">
+			<div class="col-md-12 col-sm-12 margin30">
+                <p>&copy; 2016 Apptricity Corporation</p>
+            </div>
+        </footer>
+        <!--default footer end here-->
+
